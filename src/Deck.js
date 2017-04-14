292789343778fe25aa755259b0ac0e780ff0ecee
GarenLiang <ideabreeze@gmail.com>
2017-04-14 16:53:41 -0700

    Swipe Right Clear

--- src/Deck.js
+++ src/Deck.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { View, Animated, PanResponder, Dimensions } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
-
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 class Deck extends Component {
   constructor(props) {
     super(props);
@@ -12,9 +13,28 @@ class Deck extends Component {
       onPanResponderMove: (event, gesture) => {
         position.setValue({ x: gesture.dx, y: gesture.dy });
       },
-      onPanResponderRelease: () => {}
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+          this.forceSwipeRight();
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+          console.log('swipe left!');
+        } else {
+          this.resetPosition();
+        }
+      }
     });
     this.state = { panResponder, position };
+  }
+  forceSwipeRight() {
+    Animated.timing(this.state.position, {
+      toValue: { x: SCREEN_WIDTH, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start();
+  }
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
   }
   getCardStyle() {
     const { position } = this.state;
